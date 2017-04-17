6817dcfff91735fc23823ebba4f847c6f813e20e
Ieshea Goree <iesheagoree@gmail.com>
2017-04-17 17:57:12 -0500

    -working progress

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -41,7 +41,8 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+            return "new-job";
 
     }
 }
